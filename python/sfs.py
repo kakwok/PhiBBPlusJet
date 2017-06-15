be641ec3c33ffb768b2fcfe9648abdcb3b82d05e
DryRun <david.renhwa.yu@gmail.com>
2017-06-15 18:17:54 +0200

    Add cuts and SF storage

---
+++ python/sfs.py
@@ -0,0 +1,15 @@
+sfs = {}
+sfs["AK8"] = {
+	"BB":0.91,
+	"BB_ERR":0.03,
+	"V":0.993,
+	"V_ERR":0.043
+}
+
+# WARNING : CA15 currently just copied from AK8. These need to be re-derived!
+sfs["CA15"] = {
+	"BB":0.91,
+	"BB_ERR":0.03,
+	"V":0.993,
+	"V_ERR":0.043
+}
